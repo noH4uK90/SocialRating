0604912a3e4cc627503d5e9fc84dac2d89ff0ae1
noH4uK <noh4uk90@gmail.com>
2024-10-23 14:34:09 +0300

    Add prepareConnection and prepateStatement

--- src/main/kotlin/org/example/noh4uk/socialRating/core/db/Database.kt
+++ src/main/kotlin/org/example/noh4uk/socialRating/core/db/Database.kt
@@ -1,6 +1,7 @@
 package org.example.noh4uk.socialRating.core.db
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import net.luckperms.api.model.user.User
 import org.example.noh4uk.socialRating.SocialRating
@@ -9,6 +10,7 @@ import org.example.noh4uk.socialRating.core.models.PagedList
 import org.example.noh4uk.socialRating.core.models.RatingHistory
 import java.sql.Connection
 import java.sql.Date
+import java.sql.PreparedStatement
 import java.sql.SQLException
 import java.text.SimpleDateFormat
 import java.util.UUID
@@ -21,30 +23,19 @@ interface Database {
         rating: Int,
         reason: String
     ) = withContext(Dispatchers.IO) {
-        getConnection().use { connection ->
-            try {
-                connection.autoCommit = false
-                addHistory(player, changerPlayer, rating, reason, ChangingRatingType.Add)
-                connection.prepareStatement("""
-                    UPDATE player_rating
-                    SET current_rating = current_rating + ?
-                    WHERE player_id = ?
-                """.trimIndent()).use { statement ->
-                    statement.setInt(1, rating)
-                    statement.setString(2, player.uniqueId.toString())
-                    val changedRows = statement.executeUpdate()
+        prepareStatement("""
+            UPDATE player_rating
+            SET current_rating = current_rating + ?
+            WHERE player_id = ?
+        """.trimIndent()) { connection, statement ->
+            statement.setInt(1, rating)
+            statement.setString(2, player.uniqueId.toString())
+            val changedRows = statement.executeUpdate()
 
-                    if (changedRows > 0) {
-                        connection.commit()
-                    } else {
-                        connection.rollback()
-                    }
-                }
-            } catch (e: SQLException) {
+            if (changedRows > 0) {
+                connection.commit()
+            } else {
                 connection.rollback()
-            }
-            finally {
-                connection.autoCommit = true
             }
         }
     }
@@ -55,48 +46,35 @@ interface Database {
         rating: Int,
         reason: String,
     ) = withContext(Dispatchers.IO) {
-        getConnection().use { connection ->
-            try {
-                connection.autoCommit = false
-                addHistory(player, changerPlayer, rating, reason, ChangingRatingType.Remove)
-                connection.prepareStatement("""
-                    UPDATE player_rating
-                    SET current_rating = current_rating - ?
-                    WHERE player_id = ?
-                """.trimIndent()).use { statement ->
-                    statement.setInt(1, rating)
-                    statement.setString(2, player.uniqueId.toString())
-                    val changedRows = statement.executeUpdate()
+        prepareStatement("""
+            UPDATE player_rating
+            SET current_rating = current_rating - ?
+            WHERE player_id = ?
+        """.trimIndent()) { connection, statement ->
+            statement.setInt(1, rating)
+            statement.setString(2, player.uniqueId.toString())
+            val changedRows = statement.executeUpdate()
 
-                    if (changedRows > 0) {
-                        connection.commit()
-                    } else {
-                        connection.rollback()
-                    }
-                }
-            } catch (e: SQLException) {
+            if (changedRows > 0) {
+                connection.commit()
+            } else {
                 connection.rollback()
-            }
-            finally {
-                connection.autoCommit = true
             }
         }
     }
 
     suspend fun getCurrentRating(player: User): Int? = withContext(Dispatchers.IO) {
-        getConnection().use { connection ->
-            connection.prepareStatement("""
+        prepareStatement("""
                 SELECT current_rating
                 FROM player_rating
                 WHERE player_id = ?
-            """).use { statement ->
-                statement.setString(1, player.uniqueId.toString())
-                val result = statement.executeQuery()
-                if (result.next()) {
-                    result.getInt("current_rating")
-                } else {
-                    null
-                }
+            """.trimIndent()) { _, statement ->
+            statement.setString(1, player.uniqueId.toString())
+            val result = statement.executeQuery()
+            if (result.next()) {
+                result.getInt("current_rating")
+            } else {
+                null
             }
         }
     }
@@ -108,7 +86,7 @@ interface Database {
         val pageSize = 3
         val offset = (page - 1) * pageSize
 
-        getConnection().use { connection ->
+        return prepareConnection { connection ->
             val countStatement = connection.prepareStatement(
                 "SELECT COUNT(*) FROM rating_history WHERE player_id = ?"
             )
@@ -145,7 +123,7 @@ interface Database {
 
             val totalPages = (totalItems + pageSize - 1) / pageSize
 
-            return PagedList(
+            PagedList(
                 page = page,
                 pageSize = pageSize,
                 totalPages = totalPages,
@@ -156,34 +134,22 @@ interface Database {
     }
 
     suspend fun addPlayerRatingIfNotExists(player: User, rating: Int = 100) = withContext(Dispatchers.IO) {
-        getConnection().use { connection ->
-            try {
-                connection.autoCommit = false
-                connection.prepareStatement(
-                    """
-                INSERT INTO player_rating (id, player_id, username, current_rating)
-                SELECT ?, ?, ?, ?
-                WHERE NOT EXISTS (
-                    SELECT 1 FROM player_rating WHERE player_id = ?
-                )
-            """
-                ).use { statement ->
-                    statement.setString(1, UUID.randomUUID().toString())
-                    statement.setString(2, player.uniqueId.toString())
-                    statement.setString(3, player.username)
-                    statement.setInt(4, rating)
-                    statement.setString(5, player.uniqueId.toString())
+        prepareStatement("""
+            INSERT INTO player_rating (id, player_id, username, current_rating)
+            SELECT ?, ?, ?, ?
+            WHERE NOT EXISTS (
+                SELECT 1 FROM player_rating WHERE player_id = ?
+            )
+        """.trimIndent()) { connection, statement ->
+            statement.setString(1, UUID.randomUUID().toString())
+            statement.setString(2, player.uniqueId.toString())
+            statement.setString(3, player.username)
+            statement.setInt(4, rating)
+            statement.setString(5, player.uniqueId.toString())
 
-                    val rowsAffected = statement.executeUpdate()
-                    if (rowsAffected > 0) {
-                        connection.commit()
-                    }
-                }
-            } catch (e: SQLException) {
-                connection.rollback()
-            }
-            finally {
-                connection.autoCommit = true
+            val rowsAffected = statement.executeUpdate()
+            if (rowsAffected > 0) {
+                connection.commit()
             }
         }
     }
@@ -199,42 +165,31 @@ interface Database {
         reason: String,
         type: ChangingRatingType
     ) = withContext(Dispatchers.IO) {
-        getConnection().use { connection ->
-            try {
-                connection.autoCommit = false
-                connection.prepareStatement("""
-                    INSERT INTO rating_history (id, date, player_id, player_id_changer, player_changer, count, reason, type)
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-                """).use { statement ->
-                    statement.setString(1, UUID.randomUUID().toString())
-                    statement.setString(2, SimpleDateFormat("dd.MM.yyyy").format(Date(System.currentTimeMillis())))
-                    statement.setString(3, player.uniqueId.toString())
-                    statement.setString(4, changerPlayer.uniqueId.toString())
-                    statement.setString(5, changerPlayer.username)
-                    statement.setInt(6, rating)
-                    statement.setString(7, reason)
-                    statement.setString(8, type.name)
-                    val updatedRows = statement.executeUpdate()
-                    if (updatedRows > 0) {
-                        connection.commit()
-                    } else {
-                        connection.rollback()
-                    }
-                }
-            } catch (e: SQLException) {
+        prepareStatement("""
+            INSERT INTO rating_history (id, date, player_id, player_id_changer, player_changer, count, reason, type)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        """.trimIndent()) { connection, statement ->
+            statement.setString(1, UUID.randomUUID().toString())
+            statement.setString(2, SimpleDateFormat("dd.MM.yyyy").format(Date(System.currentTimeMillis())))
+            statement.setString(3, player.uniqueId.toString())
+            statement.setString(4, changerPlayer.uniqueId.toString())
+            statement.setString(5, changerPlayer.username)
+            statement.setInt(6, rating)
+            statement.setString(7, reason)
+            statement.setString(8, type.name)
+
+            val updatedRows = statement.executeUpdate()
+            if (updatedRows > 0) {
+                connection.commit()
+            } else {
                 connection.rollback()
-            }
-            finally {
-                connection.autoCommit = true
             }
         }
     }
 
-    fun createSQLiteTableIfNotExists() {
-        val connection = getConnection()
-        connection.autoCommit = false
-        connection.use { conn ->
-            val statement = conn.createStatement()
+    fun createSQLiteTableIfNotExists() = runBlocking {
+        prepareConnection { connection ->
+            val statement = connection.createStatement()
             statement.execute("""
                 CREATE TABLE IF NOT EXISTS player_rating (
                     id TEXT PRIMARY KEY,
@@ -266,12 +221,10 @@ interface Database {
         }
     }
 
-    fun createMySQLTableIfNotExists() {
-        val connection = getConnection()
-        connection.autoCommit = false
-        connection.use { conn ->
-            val statement = conn.createStatement()
-            val players = statement.execute("""
+    fun createMySQLTableIfNotExists() = runBlocking {
+        prepareConnection { connection ->
+            val statement = connection.createStatement()
+            statement.execute("""
                 CREATE TABLE IF NOT EXISTS player_rating (
                     id VARCHAR(36) PRIMARY KEY,
                     player_id VARCHAR(36) UNIQUE NOT NULL,
@@ -279,7 +232,7 @@ interface Database {
                     current_rating INT NOT NULL DEFAULT 100
                 )
             """)
-            val history = statement.execute("""
+            statement.execute("""
                 CREATE TABLE IF NOT EXISTS rating_history (
                     id VARCHAR(36) PRIMARY KEY,
                     date VARCHAR(255) NOT NULL,
@@ -292,8 +245,30 @@ interface Database {
                     FOREIGN KEY (player_id) REFERENCES player_rating(player_id) ON UPDATE CASCADE
                 )
             """)
-            SocialRating.getInstance().logger.info("Tables players: $players, history: $history")
             connection.commit()
+        }
+    }
+
+    private suspend fun <T> prepareConnection(completion: (connection: Connection) -> T) = withContext(Dispatchers.IO) {
+        getConnection().use { connection ->
+            try {
+                connection.autoCommit = false
+                completion(connection)
+            } catch (e: SQLException) {
+                connection.rollback()
+                throw e
+            }
+            finally {
+                connection.autoCommit = true
+            }
+        }
+    }
+
+    private suspend fun <T> prepareStatement(sql: String, completion: (connection: Connection, statement: PreparedStatement) -> (T)) = withContext(Dispatchers.IO) {
+        prepareConnection { connection ->
+            connection.prepareStatement(sql).use { statement ->
+                completion(connection, statement)
+            }
         }
     }
 }
